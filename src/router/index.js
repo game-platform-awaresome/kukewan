5e039b7635fffa8d594860d93dfde1681569d62f
greentea <741024920@qq.com>
2017-12-07 16:35:00 +0800

    init

---
+++ src/router/index.js
@@ -0,0 +1,43 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import Index from 'components/index/index'
+import GameHall from 'components/game-hall/game-hall'
+import Pay from 'components/pay/pay'
+import User from 'components/user/user'
+import IntegralMall from 'components/integral-mall/integral-mall'
+import CustomerService from 'components/customer-service/customer-service'
+
+Vue.use(Router)
+
+export default new Router({
+  routes: [
+    {
+      path: '/',
+      redirect: '/index'
+    },
+    {
+      path: '/index',
+      component: Index
+    },
+    {
+      path: '/game-hall',
+      component: GameHall
+    },
+    {
+      path: '/pay',
+      component: Pay
+    },
+    {
+      path: '/user',
+      component: User
+    },
+    {
+      path: '/integral-mall',
+      component: IntegralMall
+    },
+    {
+      path: '/customer-service',
+      component: CustomerService
+    }
+  ]
+})
